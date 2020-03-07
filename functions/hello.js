f899bba4583be0da65ab6bf50e0a1797a3f4cc61
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-03-07 08:52:26 -0600

    add hello function

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+// functions/hello.js
+exports.handler = async event => {
+  const subject = event.queryStringParameters.name || 'world'
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`,
+  }
+}
